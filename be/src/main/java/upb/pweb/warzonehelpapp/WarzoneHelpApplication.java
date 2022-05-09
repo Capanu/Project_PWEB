175cc93b4cd6f6dd70526a078c367366f9990557
ManuTrt <emanuelthegamer@gmail.com>
2022-05-09 20:01:01 +0300

    be init done + mysql db setup + init folders + CORS config

---
+++ be/src/main/java/upb/pweb/warzonehelpapp/WarzoneHelpApplication.java
@@ -0,0 +1,13 @@
+package upb.pweb.warzonehelpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarzoneHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarzoneHelpApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/upb/pweb/warzonehelpapp/WarzonehelpappApplicationTests.java
@@ -0,0 +1,13 @@
+package upb.pweb.warzonehelpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarzonehelpappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
