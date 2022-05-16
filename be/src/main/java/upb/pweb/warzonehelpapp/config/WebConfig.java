6d341cdfeb1822cbd9e0a32592b6dfe07a88992b
ManuTrt <emanuelthegamer@gmail.com>
2022-05-16 17:51:13 +0300

    done:  - added email sender

--- be/src/main/java/upb/pweb/warzonehelpapp/config/WebConfig.java
+++ be/src/main/java/upb/pweb/warzonehelpapp/config/WebConfig.java
@@ -2,6 +2,7 @@ package upb.pweb.warzonehelpapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,6 +11,7 @@ import upb.pweb.warzonehelpapp.config.filter.AuthorizeRoleRequestInterceptor;
 
 @Configuration
 @EnableWebMvc
+@EnableAsync
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- be/src/main/java/upb/pweb/warzonehelpapp/controller/public_api/DonateController.java
+++ be/src/main/java/upb/pweb/warzonehelpapp/controller/public_api/DonateController.java
@@ -2,11 +2,13 @@ package upb.pweb.warzonehelpapp.controller.public_api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import upb.pweb.warzonehelpapp.controller.internal_api.resources.BasicSuccessResponse;
 import upb.pweb.warzonehelpapp.controller.public_api.resources.NewDonationRequest;
 import upb.pweb.warzonehelpapp.exception.InvalidCampaignException;
+import upb.pweb.warzonehelpapp.service.EmailSenderService;
 import upb.pweb.warzonehelpapp.service.base.FundraisingCampaignService;
 
 import javax.validation.Valid;
@@ -17,6 +19,8 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class DonateController {
     private final FundraisingCampaignService fundraisingCampaignService;
+
+    private final EmailSenderService service;
 
     @PostMapping("/new-donation")
     public ResponseEntity<?> newDonation(@RequestBody @Valid NewDonationRequest request) throws InvalidCampaignException {
